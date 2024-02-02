b7c5b2d26001ed668bfb5166a093142683637c6f
ankit-dslr <ankit.karn@dslrteam.com>
2024-02-02 22:17:57 +0530

    Added assets and utitlity files

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { Room } from "./Room";
 
 export default function Page() {
   return (
-    <h1 className="text-5xl text-white">FigiTool</h1>
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="text-5xl text-white">FigiTool</h1>
+    </div>
   );
 }
