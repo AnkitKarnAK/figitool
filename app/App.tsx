56e3f669eebe801072dda628d5f8f22b2b3a7337
ankit-dslr <ankit.karn@dslrteam.com>
2024-02-03 00:43:09 +0530

    disable ssr to deploy on vercel

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Live from "@/components/Live";
+import { Room } from "./Room";
+
+export default function App() {
+    return (
+        <div >
+            <Live />
+        </div>
+    );
+}
